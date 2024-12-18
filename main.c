4b5e21f62cbcac98b5e0ede7df0fbe6993626436
PMichalkiewicz <255656@edu.p.lodz.pl>
2024-12-18 14:57:02 +0100

    zmiana na mnożenie w metodzie commit

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+int policz(int a, int b)
+{
+    return a*b;
+}
 int main(void)
 {
     printf("Hello, World!\n");
+    printf("W drugim branchu: 2*3 = %d", policz(2, 3));
     return 0;
 }
